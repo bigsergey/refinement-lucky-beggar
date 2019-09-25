879d12f0bec0d87ff6399f466358563d8fb35e23
Sergey Bolshov <sergey.bolshov@allegro.pl>
2019-09-25 18:17:27 +0200

    master Add hero component

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,17 +1,21 @@
 <template>
-  <section class="section">
-    <h1>Welcome to our meeting!</h1>
-    <participant-list v-bind:list="participantList"></participant-list>
-    <add-participant :onAdded="onParticipantAdded"></add-participant>
+  <section>
+    <hero title="Welcome on our meeting!" subtitle="Lets generate next lucky beggar" />
+
+    <section class="container">
+      <participant-list v-bind:list="participantList"></participant-list>
+      <add-participant :onAdded="onParticipantAdded"></add-participant>
+    </section>
   </section>
 </template>
 
 <script>
 import AddParticipant from './AddParticipant';
 import ParticipantList from './ParticipantList';
+import Hero from './Hero';
 
 export default {
-  components: { AddParticipant, ParticipantList },
+  components: { AddParticipant, ParticipantList, Hero },
   data() {
     return {
       participantList: ['First Person', 'Second Person'],
