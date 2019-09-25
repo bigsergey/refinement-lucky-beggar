3d437a4d1bc7b4e09a51962ed11c5ba814045197
Sergey Bolshov <sergey.bolshov@allegro.pl>
2019-09-25 19:33:20 +0200

    master Generate lucky beggar random list

--- src/renderer/components/RandomizerTile/index.vue
+++ src/renderer/components/RandomizerTile/index.vue
@@ -8,7 +8,12 @@
         type="is-primary"
         size="is-medium"
         icon-right="account-convert"
+        @click="getRandomItem"
       >Generate</b-button>
+
+      <ul v-if="luckyBeggarList.length > 0">
+        <li v-for="(item, index) in luckyBeggarList" v-bind:key="index">{{item}}</li>
+      </ul>
     </div>
   </section>
 </template>
@@ -18,5 +23,18 @@ export default {
   props: {
     participantList: { type: Array },
   },
+  data() {
+    return {
+      luckyBeggarList: [],
+    };
+  },
+  methods: {
+    getRandomItem() {
+      const maxIndex = this.participantList.length;
+      const randomIndex = Math.floor(Math.random() * maxIndex);
+      this.luckyBeggarList.push(this.participantList[randomIndex]);
+    },
+  },
 };
 </script>
+
