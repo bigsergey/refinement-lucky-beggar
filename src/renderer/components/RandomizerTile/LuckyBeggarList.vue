dc49a528c07c105698bb06cbfb7e8646e02132aa
Sergey Bolshov <sergey.bolshov@allegro.pl>
2019-09-29 19:41:00 +0200

    random-participant Display only last four laucky beggars

--- src/renderer/components/RandomizerTile/LuckyBeggarList.vue
+++ src/renderer/components/RandomizerTile/LuckyBeggarList.vue
@@ -1,8 +1,10 @@
 <template>
   <ul v-if="luckyBeggarList.length > 0">
-    <li v-for="(item, index) in luckyBeggarList" v-bind:key="index">
-      {{item}}
-    </li>
+    <li
+      v-for="(item, index) in getLastRecords(luckyBeggarList)"
+      v-bind:key="index"
+    >{{item}}</li>
+    <li v-if="luckyBeggarList.length > lastRecordNumber">...</li>
   </ul>
 </template>
 
@@ -10,6 +12,15 @@
 export default {
   props: {
     luckyBeggarList: { type: Array },
+    lastRecordNumber: {
+      type: Number,
+      default: 4,
+    },
+  },
+  methods: {
+    getLastRecords() {
+      return this.luckyBeggarList.slice(0, this.lastRecordNumber);
+    },
   },
 };
 </script>
